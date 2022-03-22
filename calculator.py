b303cb0eebbe8e6d04c995e8838fd92c2c40cbb9
nevasepracheta <97940662+nevasepracheta@users.noreply.github.com>
2022-03-22 18:07:19 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,17 +1,17 @@
-num1 =  input("Enter a number1: ")
-num2 =  input("Enter a number2: ")
+num1 =  float(input("Enter a number1: "))
+num2 =  float(input("Enter a number2: "))
 
 print("For addition; Type Add\nFor subtraction; Type Sub\nFor Multiplication; Type Prod\nFor Division; Type Div\n")
 userinput = input("Enter your choice: ")
 print(userinput)
 
 if userinput.lower() ==  "add":
-        print(float(num1) + float(num2))
+        print(num1 + num2)
 elif userinput.lower() == "sub":
-        print(float(num1) - float(num2))
+        print(num1 - num2)
 elif userinput.lower() == "prod":
-        print(float(num1)*float(num2))
+        print(num1*num2)
 elif userinput.lower() == "div":
-        print(float(num1) // float(num2))
+        print(num1//num2)
 else:
         print("Invalid operation")
