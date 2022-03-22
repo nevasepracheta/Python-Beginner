a551b9e5a72a5be55382f81bc2fc12d068cfab59
nevasepracheta <97940662+nevasepracheta@users.noreply.github.com>
2022-03-22 18:08:51 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,10 @@
+def say_hi(name):  # Declaration
+
+    print("Hello " + name)
+
+def add_funct(num1, num2):
+   return num1 + num2  # Return statement
+# Code breaks after return statement. Nothing will be executed after return statement****
+
+say_hi("Mike")  # Calling the function
+print(add_funct(2,3)) # Function call also print out the return value
