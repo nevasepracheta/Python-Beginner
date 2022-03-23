e1eb2289942b6653905962f89c2fd3bebd1855cb
nevasepracheta <97940662+nevasepracheta@users.noreply.github.com>
2022-03-23 18:21:20 +0530

    Create student_object_example.py

---
+++ student_object_example.py
@@ -0,0 +1,7 @@
+from student import student_info
+
+student_1 = student_info("Jim","Business",3.5, False)
+student_2 = student_info("Pam","Art",2.9, True)
+
+print(student_1.name)
+print(student_1.on_honor_roll())
